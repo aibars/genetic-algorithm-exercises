e02131e1ba295f980a29d488230580968439a9d4
Agustín Ibars <agustin.ibars@gmail.com>
2020-01-09 16:00:13 -0300

    linting

--- mochila/MOCHILA-GREEDY.C
+++ mochila/MOCHILA-GREEDY.C
@@ -1,7 +1,7 @@
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #define VOL 4200
 void carga();
 void ordenar();
@@ -10,8 +10,7 @@ float elementos[10][3];
 float densidad[10];
 float elemento[8];
 
-main()
-{
+main() {
   int i;
   clrscr();
   carga();
@@ -24,12 +23,10 @@ main()
   getche();
 }
 
-void carga()
-{
+void carga() {
   int i;
 
-  for (i = 0; i < 10; i++)
-  {
+  for (i = 0; i < 10; i++) {
     printf("Ingrese Volumen del elemento %d: ", (i + 1));
     scanf("%f", &elementos[i][0]);
     printf("Ingrese Valor del elemento %d: ", (i + 1));
@@ -37,22 +34,19 @@ void carga()
     elementos[i][2] = ((elementos[i][1]) / (elementos[i][0]));
   }
   for (i = 0; i < 10; i++)
-    printf("volumen %f valor %f densidad %f \n", elementos[i][0], elementos[i][1], elementos[i][2]);
+    printf("volumen %f valor %f densidad %f \n", elementos[i][0],
+           elementos[i][1], elementos[i][2]);
 }
-void ordenar()
-{
+void ordenar() {
   float aux[3];
   int i, j, x = 0;
 
-  for (j = 0; j < 10; j++)
-  {
+  for (j = 0; j < 10; j++) {
     aux[0] = elementos[x][0];
     aux[1] = elementos[x][1];
     aux[2] = elementos[x][2];
-    for (i = x; i < 10; i++)
-    {
-      if (elementos[i][2] > aux[2])
-      {
+    for (i = x; i < 10; i++) {
+      if (elementos[i][2] > aux[2]) {
         elementos[x][0] = elementos[i][0];
         elementos[x][1] = elementos[i][1];
         elementos[x][2] = elementos[i][2];
@@ -69,17 +63,15 @@ void ordenar()
 
   printf("\n");
   for (i = 0; i < 10; i++)
-    printf("volumen %f valor %f densidad %f \n", elementos[i][0], elementos[i][1], elementos[i][2]);
+    printf("volumen %f valor %f densidad %f \n", elementos[i][0],
+           elementos[i][1], elementos[i][2]);
 }
 
-void seleccionar()
-{
+void seleccionar() {
   int auxm, min, dif = 4200, i = 0, mochila = 0, precio = 0;
   min = elementos[9][0];
-  do
-  {
-    if (elementos[i][0] < dif)
-    {
+  do {
+    if (elementos[i][0] < dif) {
       mochila = mochila + elementos[i][0];
       precio = precio + elementos[i][1];
       dif = VOL - mochila;
